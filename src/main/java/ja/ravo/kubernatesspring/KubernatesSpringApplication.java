235037d8c4ae77bafb6156e53f12e26b1ab8bec9
RAVOMANANA <jeanaime.ravomanana@gmail.com>
2024-12-08 20:09:41 +0400

    initial commit

---
+++ src/main/java/ja/ravo/kubernatesspring/KubernatesSpringApplication.java
@@ -0,0 +1,13 @@
+package ja.ravo.kubernatesspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernatesSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubernatesSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ja/ravo/kubernatesspring/KubernatesSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ja.ravo.kubernatesspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernatesSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
